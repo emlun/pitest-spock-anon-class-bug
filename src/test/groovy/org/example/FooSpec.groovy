d126442e1d85283b8a260854f0ba1a813e3a6972
Emil Lundberg <emil.lundberg@sunstone.se>
2017-01-10 19:43:53 +0100

    Add test

--- src/test/groovy/org/example/FooSpec.groovy
+++ src/test/groovy/org/example/FooSpec.groovy
@@ -6,4 +6,9 @@ class FooSpec extends Specification {
 
   private static final Foo foo = new Foo() {}
 
+  def 'getRandomInteger always returns 4.'() {
+    expect:
+      foo.getRandomInteger() == 4
+  }
+
 }

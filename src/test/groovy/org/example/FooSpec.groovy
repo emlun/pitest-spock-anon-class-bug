4fbeb1723284b7e88f44250adcb70cfa5863ed47
Emil Lundberg <emil.lundberg@sunstone.se>
2017-01-10 19:17:02 +0100

    Add bug workaround

--- src/test/groovy/org/example/FooSpec.groovy
+++ src/test/groovy/org/example/FooSpec.groovy
@@ -2,9 +2,11 @@ package org.example
 
 import spock.lang.Specification
 
+class SubFoo extends Foo {}
+
 class FooSpec extends Specification {
 
-  private static final Foo foo = new Foo() {}
+  private static final Foo foo = new SubFoo()
 
   def 'getRandomInteger always returns 4.'() {
     expect:
